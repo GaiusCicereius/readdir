b82c4024999b768e3a296b55138e460e2c9ca6b9
didinele <didinele.dev@gmail.com>
2021-04-02 14:05:41 +0300

    chore: initial commit

---
+++ babel.config.js
@@ -0,0 +1,16 @@
+/* eslint-ignore */
+
+module.exports = {
+  parserOpts: { strictMode: true },
+  sourceMaps: 'inline',
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: { node: 'current' },
+        modules: 'commonjs'
+      }
+    ],
+    '@babel/preset-typescript'
+  ]
+};
